18a6f16b768e80c718d5c5d9f9e5479729f85f92
jaimandal10 <44457903+jaimandal10@users.noreply.github.com>
2020-09-18 09:06:54 +0530

    Add files via upload

---
+++ build/app/generated/source/buildConfig/release/com/storesconnect/storesconnect/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.storesconnect.storesconnect;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.storesconnect.storesconnect";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.0";
+}
